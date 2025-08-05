07cac9bf98dd8ab30ef5baafaaa65894d5508ede
aiyaz418 <a4aiyaz@gmail.com>
2025-08-05 16:53:25 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginSignupForm from './components/LoginSignupForm/LoginSignupForm.jsx';
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignupForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import LoginSignupForm from "./components/LoginSignupForm/LoginSignupForm";
+import "./index.css";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import Dashboard from "./components/Dashboard"; 
+
+const router = createBrowserRouter([
+  { path: "/login", element: <LoginSignupForm /> },
+  { path: "/home", element: <Home /> },
+  { path: "/dashboard", element: <Dashboard /> }, 
+  { path: "*", element: <NotFound /> }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
